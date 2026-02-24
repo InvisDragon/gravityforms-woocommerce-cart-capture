ced893072efac24e354d73cfff45bb4325383b42
Joe Simpson <kennydude@users.noreply.github.com>
2026-02-24 14:27:37 +0000

    Update gravity-cart-capture.php
    * Updates upon submission of form
    * On email notifications using all_fields the field content is rendered correctly

--- gravity-cart-capture.php
+++ gravity-cart-capture.php
@@ -1,16 +1,14 @@
-<?php
-
 /*
  * Plugin Name:  Gravity Forms: WooCommerce Cart Capture
  * Plugin URI: https://github.com/lucasstark/gravityforms-woocommerce-cart-capture
- * Description:  Automatically capture the contents of a users cart when they submit a gravity form.  To use Set gravity form to pre-populate a field.  Set the key as woocommerce_cart.  The cart contents at the time of submission will automatically be captured. 
+ * Description:  Automatically capture the contents of a users cart when they submit a gravity form.  To use Set gravity form to pre-populate a field.  Set the key as woocommerce_cart.  The cart contents at the time of submission will automatically be captured.
  * Author:  Lucas Stark
  * Author URI:  http://www.github.com/lucasstark/
  * License: GPLv3
  */
 
 
-class GFWC_Cart_Capture {	
+class GFWC_Cart_Capture {
 	private static $instance;
 
 	public static function register() {
@@ -22,19 +20,38 @@ class GFWC_Cart_Capture {
 	private function __construct() {
 		add_filter( 'gform_field_value_woocommerce_cart', array($this, 'record_cart_item_meta') );
 		add_filter( 'gform_entry_field_value', array($this, 'display_cart_item_meta'), 10, 4 );
+		add_action( 'gform_pre_submission', array($this, 'gform_pre_submission') );
+        add_filter( 'gform_merge_tag_filter', array($this, 'gform_merge_tag_filter'), 10, 4 );
+	}
+
+    public function gform_merge_tag_filter($field_value, $merge_tag, $options, $field) {
+        if($field['inputName'] == 'woocommerce_cart') {
+            return $this->display_cart_item_meta($field_value, $field, null, null);
+        }
+        return $field_value;
+    }
+	
+	public function gform_pre_submission($form) {
+	    foreach($form['fields'] as $field) {
+	        if($field['inputName'] == 'woocommerce_cart') {
+	            $_POST['input_' . $field['id']] = $this->record_cart_item_meta(null);
+	        }
+	    }
 	}
 
 	public function record_cart_item_meta( $value ) {
 		$items = array();
+
+		if(!WC()->cart) return $value;
 
 		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 			$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 				$items[$cart_item_key] = array(
-				    'title' => apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ), 
-				    'quantity' => $cart_item['quantity'],
-				    'meta' => $this->get_cart_item_data($cart_item)
+					'title' => apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ),
+					'quantity' => $cart_item['quantity'],
+					'meta' => $this->get_cart_item_data($cart_item)
 				);
 			}
 		}
@@ -51,13 +68,13 @@ class GFWC_Cart_Capture {
 			if ( $cart_items && is_array( $cart_items ) ) :
 				foreach ( $cart_items as $cart_item ) :
 					$output .= '<li style="margin-bottom:10px;">';
-					$output .= '<strong>' . $cart_item['title'] . '</strong>: ( ' . $cart_item['quantity'] . ' )';
+					$output .= '<strong>' . $cart_item['title'] . '</strong>: ( qty ' . $cart_item['quantity'] . ' )';
 
 					if ( isset( $cart_item['meta'] ) ) :
 						$output .= '<ul style="margin-left:15px;margin-top:5px;">';
 						foreach ( $cart_item['meta'] as $cart_item_meta ) :
-							$output .= '<li><strong>' . $cart_item_meta['key'] . '</strong>: ' . $cart_item_meta['value'] . '</li>';
-						endforeach;	
+							$output .= '<li><strong>' . $cart_item_meta['key'] . '</strong>: ' . $cart_item_meta['display'] . '</li>';
+						endforeach;
 						$output .= '</ul>';
 					endif;
 
@@ -101,15 +118,18 @@ class GFWC_Cart_Capture {
 					$value = apply_filters( 'woocommerce_variation_option_name', $value );
 					$product_attributes = $cart_item['data']->get_attributes();
 					if ( isset( $product_attributes[str_replace( 'attribute_', '', $name )] ) ) {
-						$label = wc_attribute_label( $product_attributes[str_replace( 'attribute_', '', $name )]['name'] );
+						$label = $product_attributes[str_replace( 'attribute_', '', $name )];
+						if(is_array($label)) {
+							$label = wc_attribute_label($label['name']);
+						}
 					} else {
 						$label = $name;
 					}
 				}
 
 				$item_data[] = array(
-				    'key' => $label,
-				    'value' => $value
+					'key' => $label,
+					'value' => $value
 				);
 			}
 		}
@@ -127,7 +147,7 @@ class GFWC_Cart_Capture {
 			$item_data[$key]['key'] = !empty( $data['key'] ) ? $data['key'] : $data['name'];
 			$item_data[$key]['display'] = !empty( $data['display'] ) ? $data['display'] : $data['value'];
 		}
-		
+
 		return $item_data;
 	}
 
